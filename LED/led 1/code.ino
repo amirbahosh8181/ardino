481cce35de2b9ac8e9ca14d470b67dbfd28b0454
amirbahosh8181 <hajamir7766@gmail.com>
2025-07-17 03:04:43 -0700

    add projects

---
+++ LED/led 1/code.ino
@@ -0,0 +1,9 @@
+int ledPin = 8 :
+int val:
+void setup() (
+pinMode (LED)
+_BUILTIN, OUTPUT) :
+pinkode (ledPin, INPUT):
+void 100p() (
+val = digitalRead (ledPin) :
+digitalkrite (LED_BUILTE, val) :
--- led 1/code.ino
+++
@@ -1,9 +0,0 @@
-int ledPin = 8 :
-int val:
-void setup() (
-pinMode (LED)
-_BUILTIN, OUTPUT) :
-pinkode (ledPin, INPUT):
-void 100p() (
-val = digitalRead (ledPin) :
-digitalkrite (LED_BUILTE, val) :
